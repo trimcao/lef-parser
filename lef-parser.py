b2a44e2a8d0431181ec68fe88f142587b28d0d54
Tri Minh Cao <trimcao@gmail.com>
2016-08-11 13:37:13 -0500

    created util.py that stores data structures

--- lef-parser.py
+++ lef-parser.py
@@ -5,7 +5,7 @@ Email: tricao@utdallas.edu
 Date: August 2016
 """
 
-f = open('/Users/trimcao/Dropbox/Research/2016/split-manufacturing/libraries/FreePDK45/gscl45nm.lef', 'r+')
+f = open('./libraries/FreePDK45/FreePDK45nm.lef', 'r+')
 
 for line in f:
     print (line)
---
+++ util.py
@@ -0,0 +1,6 @@
+"""
+Data Structures for LEF Parser
+Author: Tri Minh Cao
+Email: tricao@utdallas.edu
+Date: August 2016
+"""
