86b9106d554fd9c7bdab2b1c83c9c5bf3a0e67b2
Tri Minh Cao <trimcao@gmail.com>
2016-08-12 15:38:50 -0500

    basic structure of LEF parser done, need complete data structures for MACRO statement.

--- lef-parser.py
+++ lef-parser.py
@@ -22,19 +22,48 @@ def strToList(s):
         result.pop()
     return result
 
-#f = open("./libraries/FreePDK45/FreePDK45nm.lef", 'r+')
-#for line in f:
-#    print (line)
-#f.close()
-
-a = Macro('helloWorld')
-#print (a.type)
-#a.parseNext('s')
-
-b = Macro('goodbye')
-
 # can make the stack to be an object if needed
 stack = []
 
+# store the statements info in a list
+statements = []
+
+# Now try using my data structure to parse
+# open the file and start reading
+path = "./libraries/FreePDK45/small.lef"
+f = open(path, "r+")
+# the program will run until the end of file f
+for line in f:
+    #print (stack)
+    info = strToList(line)
+    # check if the program is processing a statement
+    if (len(stack) != 0):
+        curState = stack[len(stack) - 1]
+        nextState = curState.parseNext(info)
+    else:
+        curState = Statement()
+        nextState = curState.parseNext(info)
+    # check the status return from parseNext function
+    if (nextState == 0):
+        # continue as normal
+        pass
+    elif (nextState == 1):
+        # remove the done statement from stack, and add it to the statements
+        # list
+        if (len(stack) != 0):
+            statements.append(stack.pop())
+    elif (nextState == -1):
+        pass
+    else:
+        stack.append(nextState)
+    #print (nextState)
+f.close()
+
+# print parsed statements
+# be careful: this printing might include both parent and children objects,
+# probably I need to separate them
+print (statements)
+for each in statements:
+    print (each.toString())
 
 
--- util.py
+++ util.py
@@ -15,21 +15,131 @@ class Statement:
     def parseNext(self, data):
         """
         Method to add information from a statement from LEF file to the
-        dictionary.
+        Statement object.
         :param data: a list of strings that contains pieces of information
-        :return: 1 if everything is good, -1 if error, otherwise, return the
+        :return: 1 if parsing is done, -1 if error, otherwise, return the
         object that will be parsed next.
         """
-        print ("You are a piece of shite")
-        return 1
+        # right now, the program assumes the syntax of LEF file is correct
+        if data[0] == "MACRO":
+            name = data[1]
+            newState = Macro(name)
+            return newState
+        elif data[0] == "END":
+            return 1
+        return 0
+
+    def toString(self):
+        """
+        turn a statement object into string
+        :return: string representation of Statement objects
+        """
+        s = ""
+        s += self.type + " " + self.name + "\n"
+        for key in self.info:
+            s += "    " + key + ": " + str(self.info[key]) + "\n"
+        return s
 
 class Macro(Statement):
     """
-    Macro class to represent the a MACRO (cell) in the LEF file.
+    Macro class represents a MACRO (cell) in the LEF file.
     """
     def __init__(self, name):
         # initiate the Statement superclass
         Statement.__init__(self)
         self.type = 'MACRO'
         self.name = name
+        # other info is stored in this dictionary
+        self.info = {}
+
+    def parseNext(self, data):
+        """
+        Method to add information from a statement from LEF file to a Macro
+        object.
+        :param data: a list of strings that contains pieces of information
+        :return: 0 if in progress, 1 if parsing is done, -1 if error,
+        otherwise, return the
+        object that will be parsed next.
+        """
+        if data[0] == "CLASS":
+            self.info["CLASS"] = data[1]
+        elif data[0] == "ORIGIN":
+            xCor = float(data[1])
+            yCor = float(data[2])
+            self.info["ORIGIN"] = (xCor, yCor)
+        elif data[0] == "SIZE":
+            width = float(data[1])
+            height = float(data[3])
+            self.info["SIZE"] = (width, height)
+        elif data[0] == "SITE":
+            self.info["SITE"] = data[1]
+        elif data[0] == "OBS":
+            newObs = Obs()
+            self.info["OBS"] = newObs
+            return newObs
+        elif data[0] == "END":
+            if data[1] == self.name:
+                return 1
+            else:
+                return -1
+        return 0
+
+    # eventually I will override the toString() method
+    #def toString(self):
+
+
+class Obs(Statement):
+    """
+    Class Obs represents an OBS statement in the LEF file.
+    """
+    # Note: OBS statement does not have name
+    def __init__(self):
+        Statement.__init__(self)
+        self.type = "OBS"
+        self.name = ""
+        self.info = {}
+
+    def parseNext(self, data):
+        if data[0] == "END":
+            return 1
+        elif data[0] == "LAYER":
+            name = data[1]
+            newLayerDef = LayerDef(data[1])
+            self.info["LAYER"] = newLayerDef
+        elif data[0] == "RECT":
+            # error if the self.info["LAYER"] does not exist
+            self.info["LAYER"].addRect(data)
+        return 0
+
+class LayerDef():
+    """
+    Class LayerDef represents the Layer definition inside a PORT or OBS
+    statement.
+    """
+    # NOTE: LayerDef has no END statement
+    # I think I still need a LayerDef class, but it will not be a subclass of
+    #  Statement. It will be a normal object that stores information.
+    def __init__(self, name):
+        self.type = "LayerDef"
+        self.name = name
+        self.shapes = []
+
+    def addRect(self, data):
+        x0 = data[1]
+        y0 = data[2]
+        x1 = data[3]
+        y1 = data[4]
+        points = [(x0, y0), (x1, y1)]
+        rect = Rect(points)
+        self.shapes.append(rect)
+
+class Rect():
+    """
+    Class Rect represents a Rect definition in a LayerDef
+    """
+    # Question: Do I really need a Rect class?
+    def __init__(self, points):
+        self.points = points
+
+
 
