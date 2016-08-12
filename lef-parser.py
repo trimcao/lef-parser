8be44e588ee3bf519b5f4762bdee418237d78636
Tri Minh Cao <trimcao@gmail.com>
2016-08-12 09:43:17 -0500

    added helper strToList function

--- lef-parser.py
+++ lef-parser.py
@@ -4,10 +4,37 @@ Author: Tri Cao
 Email: tricao@utdallas.edu
 Date: August 2016
 """
+from util import *
 
-f = open('./libraries/FreePDK45/FreePDK45nm.lef', 'r+')
+"""
+Note:
+"""
 
-for line in f:
-    print (line)
+def strToList(s):
+    """
+    Function to turn a string separated by space into list of words
+    :param s: input string
+    :return: a list of words
+    """
+    result = s.split()
+    # check if the last word is ';' and remove it
+    if result[len(result) - 1] == ";":
+        result.pop()
+    return result
 
-f.close()
+#f = open("./libraries/FreePDK45/FreePDK45nm.lef", 'r+')
+#for line in f:
+#    print (line)
+#f.close()
+
+a = Macro('helloWorld')
+#print (a.type)
+#a.parseNext('s')
+
+b = Macro('goodbye')
+
+# can make the stack to be an object if needed
+stack = []
+
+
+
--- util.py
+++ util.py
@@ -4,3 +4,32 @@ Author: Tri Minh Cao
 Email: tricao@utdallas.edu
 Date: August 2016
 """
+
+class Statement:
+    """
+    General class for all types of Statements in the LEF file
+    """
+    def __init__(self):
+        pass
+
+    def parseNext(self, data):
+        """
+        Method to add information from a statement from LEF file to the
+        dictionary.
+        :param data: a list of strings that contains pieces of information
+        :return: 1 if everything is good, -1 if error, otherwise, return the
+        object that will be parsed next.
+        """
+        print ("You are a piece of shite")
+        return 1
+
+class Macro(Statement):
+    """
+    Macro class to represent the a MACRO (cell) in the LEF file.
+    """
+    def __init__(self, name):
+        # initiate the Statement superclass
+        Statement.__init__(self)
+        self.type = 'MACRO'
+        self.name = name
+
