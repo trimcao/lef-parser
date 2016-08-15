fc96f9fa6dc5152d0727232cfef8012aab3130d8
Tri Minh Cao <trimcao@gmail.com>
2016-08-15 14:21:09 -0500

    added Macro draw capability and side-by-side macro comparison.

--- lef_parser.py
+++ lef_parser.py
@@ -35,7 +35,7 @@ statements = []
 
 # Now try using my data structure to parse
 # open the file and start reading
-path = "./libraries/FreePDK45/smaller.lef"
+path = "./libraries/FreePDK45/AND2_OR2.lef"
 # path = "./libraries/FreePDK45/FreePDK45nm.lef"
 f = open(path, "r+")
 # the program will run until the end of file f
@@ -101,7 +101,7 @@ def rect_to_polygon(rect_pts):
     return poly_pt
 
 
-def draw_obs(obs):
+def draw_obs(obs, color):
     """
     Helper method to draw a OBS object
     :return: void
@@ -113,11 +113,11 @@ def draw_obs(obs):
             if (shape.type == "RECT"):
                 scaled_pts = rect_to_polygon(scaled_pts)
             draw_shape = plt.Polygon(scaled_pts, closed=True, fill=True,
-                                edgecolor='blue')
+                                color=color, edgecolor=color)
             plt.gca().add_patch(draw_shape)
 
 
-def draw_port(port):
+def draw_port(port, color):
     """
     Helper method to draw a PORT object
     :return: void
@@ -129,20 +129,59 @@ def draw_port(port):
             if (shape.type == "RECT"):
                 scaled_pts = rect_to_polygon(scaled_pts)
             draw_shape = plt.Polygon(scaled_pts, closed=True, fill=True,
-                                     color='red', edgecolor='red')
+                                     color=color, edgecolor=color)
             plt.gca().add_patch(draw_shape)
 
 
-# only draw OBS and PORT
+def draw_pin(pin):
+    """
+    function to draw a PIN object
+    :param pin: a pin object
+    :return: void
+    """
+    # chosen color of the PIN in the sketch
+
+    color = "blue"
+    pin_name = pin.name.lower()
+    if pin_name == "vdd" or pin_name == "gnd":
+        color = "blue"
+    else:
+        color = "red"
+    draw_port(pin.info["PORT"], color)
+
+def draw_macro(macro):
+    """
+    function to draw a Macro (cell) object
+    :param macro: a Macro object
+    :return: void
+    """
+    # draw OBS
+    draw_obs(macro.info["OBS"], "blue")
+    # draw each PIN
+    for pin in macro.info["PIN"]:
+        draw_pin(pin)
+
+# only draw OBS and PIN
+#for each in statements:
+#    if each.type == "OBS":
+#        draw_obs(each, "blue")
+#    elif each.type == "PIN":
+#        draw_pin(each)
+
+plt.figure(figsize=(12, 9), dpi=80)
+plt.axes()
+
+num_plot = 1
 for each in statements:
-    if each.type == "OBS":
-        draw_obs(each)
-    elif each.type == "PORT":
-        draw_port(each)
+    if each.type == "MACRO":
+        sub = plt.subplot(1, 2, num_plot)
+        # need to add title
+        sub.set_title(each.name)
+        draw_macro(each)
+        num_plot += 1
+        # scale the axis of the subplot
+        plt.axis('scaled')
 
 
 # start drawing
-plt.axes()
-plt.axis('scaled')
 plt.show()
-
